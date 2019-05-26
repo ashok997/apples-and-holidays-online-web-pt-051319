9839091ce50e90018129d4e94490c2112b7db04c
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-26 23:39:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
+
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -21,30 +23,36 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+
+  holiday_hash[:winter].each do |holiday,supplies|
+      supplies << supply
+    
+  end
 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
+
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+ 
+  supplies =[]
+  holiday_hash[:winter].each do |k,v|
+    supplies << v
+  end
+  supplies.flatten
+
 
 end
-
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
@@ -53,6 +61,21 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |seasons,holidays|
+    season =seasons.to_s.capitalize!
+    puts season << ":"
+    holidays.map do |holiday,supplies|
+    message =""
+    h = holiday.to_s.split("_")
+    h.each do |m|
+     h_string= h.join(" ")
+    end
+    message << ": "
+    s = supplies.join(", ")
+    puts message << s
+    end
+   
+  end
 
 end
 
